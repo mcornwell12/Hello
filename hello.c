33966b0217abf6f4a3500895ca9ffa3c0ea163a6
Michael Cornwell <mcornwell2@gmail.com>
2020-08-20 22:16:04 +0000

    intitial commit

---
+++ hello.c
@@ -0,0 +1,19 @@
+/**
+ *  Author: Michael Cornwell
+ * 
+ *  2020/18/08
+ * 
+ *  A simple Hello World in C
+ * 
+ */
+    #include <stdlib.h>
+    #include <stdio.h>
+    
+    int main(int argc, char **argv) {
+    
+    printf("Michael Cornwell!\n");
+    printf("Computer Science\n");
+    
+    return 0;
+    }
+    
